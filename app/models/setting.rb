b6562d3788a471457060412d604c0951ee9e7de9
vcaicedo <vcaicedo@mindsit.co>
2020-04-22 17:47:52 -0500

    [APT12.2] Inicio de implementacion estilos dinamicos

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -47,7 +47,7 @@ class Setting < ApplicationRecord
     # return default value
     case name
     when "Branding Image"
-      org_css = 'default'
+      org_css = @joiner.get_css()
       if org_css == 'claro'
         Rails.configuration.branding_image_claro
       elsif org_css == 'go'
