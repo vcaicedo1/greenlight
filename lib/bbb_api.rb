4f6a13285f3492c40740d099c3b9653fecd8b1bc
vcaicedo <vcaicedo@mindsit.co>
2020-04-24 10:26:05 -0500

    [APT14] Determinar redireccionamiento a servidor de conferencias a partir del ingreso a una sala por medio del rol del dueño de la sala

--- lib/bbb_api.rb
+++ lib/bbb_api.rb
@@ -6,11 +6,11 @@ module BbbApi
   def bbb_endpoint(user_role)
     logger.info "Dominio: #{user_role}"
     if user_role == "claro"
-      Rails.configuration.bigbluebutton_endpoint = "https://192.168.0.68/bigbluebutton/api/"
+      Rails.configuration.bigbluebutton_endpoint = "http://192.168.1.68/bigbluebutton/api/"
     elsif user_role == "gobval"
-      Rails.configuration.bigbluebutton_endpoint = "https://192.168.0.69/bigbluebutton/api/"
+      Rails.configuration.bigbluebutton_endpoint = "http://192.168.1.69/bigbluebutton/api/"
     else
-      Rails.configuration.bigbluebutton_endpoint = "https://192.168.0.67/bigbluebutton/api/"
+      Rails.configuration.bigbluebutton_endpoint = "http://192.168.1.67/bigbluebutton/api/"
     end
   end
 
