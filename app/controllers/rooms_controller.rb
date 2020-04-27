b46ab56630dfa01b0bbecdc6062de2624f52ba1a
vcaicedo <vcaicedo@mindsit.co>
2020-04-27 16:33:09 -0500

    [APT16] Implementar validacion para permitir una sala activa al tiempo por usuario.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -261,8 +261,12 @@ class RoomsController < ApplicationController
   # GET /:room_uid/logout
   def logout
     logger.info "Support: #{current_user.present? ? current_user.email : 'Guest'} has left room #{@room.uid}"
-    logger.info "Support-----1: #{current_user.present? ? current_user.id : 'No tiene'}"
-    logger.info "Support-----2: #{current_user.present? ? @room.owned_by?(current_user) : 'No es'}"
+
+    if current_user.present? ? @room.owned_by?(current_user) : false
+      logger.info "Esta saliendo el dueño de la sala"
+      @room.update_attributes(end_last_session: DateTime.now, active: false)
+    end
+
     # Redirect the correct page.
     redirect_to @room
   end
