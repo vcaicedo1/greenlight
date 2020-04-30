a8182f5f2a15b7d1a00460a86da8fc1a57a906ec
vcaicedo <vcaicedo@mindsit.co>
2020-04-30 14:22:29 -0500

    [APT18.1] Implementar tabla para historial de accesos a las salas.

--- app/models/session_history.rb
+++ app/models/session_history.rb
@@ -4,7 +4,7 @@ class SessionHistory < ApplicationRecord
 
   #Retorna el ultimo registro de sala iniciada por usuario
   def self.most_recent_for(user_id, room_id)
-    select('DISTINCT ON (room_id) *').where(user_id: user_id, room_id: room_id).order("start_session DESC").first
+    where(user_id: user_id, room_id: room_id).order("start_session DESC").first
   end
 
 end
