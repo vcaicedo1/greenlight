4718b1eb209ff29b98daff9c419d43d471316782
vcaicedo <vcaicedo@mindsit.co>
2020-05-13 12:37:53 -0500

    [APT22] Implementación de datos de organizacion a inicio de usuarios y advertencia por vencimiento de licencia.

--- app/controllers/concerns/bbb_server.rb
+++ app/controllers/concerns/bbb_server.rb
@@ -1,4 +1,3 @@
- 
 # frozen_string_literal: true
 
 # BigBlueButton open source conferencing system - http://www.bigbluebutton.org/.
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -8,4 +8,9 @@ class Organization < ApplicationRecord
   uniqueness: { case_sensitive: false, scope: :provider },
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
+  #Retorna datos organizacion
+  def self.id(organization_id)
+    Organization.where(id: organization_id)
+  end
+
 end
