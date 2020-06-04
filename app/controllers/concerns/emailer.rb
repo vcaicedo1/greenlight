43020961c0207668e8bf2a79411cc552bdb4ffd2
vcaicedo <vcaicedo@mindsit.co>
2020-06-03 22:50:06 -0500

    [APT25] Implementar permiso para enviar invitacion a usuarios

--- app/controllers/concerns/emailer.rb
+++ app/controllers/concerns/emailer.rb
@@ -131,7 +131,7 @@ module Emailer
   end
 
   def admin_emails
-    admins = User.all_users_with_roles.where(roles: { name: "name", value: "admin" })
+    admins = User.all_users_with_roles.where(roles: { name: "admin" })
 
     if Rails.configuration.loadbalanced_configuration
       admins = admins.without_role(:super_admin)
