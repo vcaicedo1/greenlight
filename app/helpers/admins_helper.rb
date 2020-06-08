64659c033daba9aa6137ebc57177aba76baad7bf
vcaicedo <vcaicedo@mindsit.co>
2020-06-08 18:23:19 -0500

    [APT25.3] Implementar permiso para enviar invitacion a usuarios al boton correspondiente.

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -26,7 +26,11 @@ module AdminsHelper
 
   def admin_invite_registration
     controller_name == "admins" && action_name == "index" &&
-      @settings.get_value("Registration Method") == Rails.configuration.registration_methods[:invite]
+      @settings.get_value("Registration Method") == Rails.configuration.registration_methods[:invite] &&
+      current_role.get_permission("can_invite_users")
+
+    jj = current_role.get_permission("can_invite_users")
+    logger.info "Probandooooooooo:#{jj}"
   end
 
   def room_authentication_string
