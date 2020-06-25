d89c746634b04cb35d12992ffe95c6657308919e
vcaicedo <vcaicedo@mindsit.co>
2020-06-25 14:22:42 -0500

    [APT24.2] Corregir problema de decodificacion de parametros con caracteres especiales decodificando B64

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -424,10 +424,10 @@ class RoomsController < ApplicationController
 
       data_uid = parameters[0]
 
-      logger.info "Accediendo desde evaluateok 1: #{CGI::unescape(parameters[1])}]"
-      logger.info "Accediendo desde evaluateok 2: #{Base64.decode64(CGI::unescape(parameters[1]))}]"
+      logger.info "Accediendo desde evaluateok 1: #{(URI.unescape(parameters[1])).force_encoding("UTF-8")}"
+      logger.info "Accediendo desde evaluateok 2: #{Base64.decode64(URI.unescape(parameters[1]))}"
 
-      data_parameters = Base64.decode64(CGI::unescape(parameters[1])).force_encoding("UTF-8")
+      data_parameters = Base64.decode64(URI.unescape(parameters[1])).force_encoding("UTF-8")
       parameters = data_parameters.split('&')
       
       data_user = parameters[0]
